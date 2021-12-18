65f01a665d5babb24288a106f775412b2dfbd4cf
Syam Kumar <yadavallisyam1@gmail.com>
2021-12-18 10:25:29 +0530

    ui complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/login.module.scss';
+import styles from '../styles/form.module.scss';
 
 import paypal from '../assets/images/paypal.png'
 import visa from '../assets/images/visa.png'
@@ -8,6 +8,17 @@ import maestro from '../assets/images/maestro.png'
 import discover from '../assets/images/discover.jpg'
 import ideal from '../assets/images/ideal.png'
 
+import inpost from '../assets/images/Delivery/inpost.png'
+import dpd from '../assets/images/Delivery/dpd.jpg'
+import dhl from '../assets/images/Delivery/dhl.png'
+import fedex from '../assets/images/Delivery/fedex.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTruck } from '@fortawesome/free-solid-svg-icons'
+
+import model1 from '../assets/images/profiles/model1.jpg'
+import model2 from '../assets/images/profiles/model2.jpg'
+
 const Login = () => {
     return (
         <div className={styles.container}>
@@ -15,9 +26,9 @@ const Login = () => {
                 <div className={styles.form_container}>
                     <div>
                         <div className={styles.ctas}>
-                            <button className={`${styles.cta} ${styles.selected}`}>Log&nbsp;&nbsp;in</button>
+                            <button type='button' className={`${styles.cta} ${styles.selected}`}>Log&nbsp;&nbsp;in</button>
                             &nbsp;&nbsp;&nbsp;
-                            <button className={styles.cta}>Sign&nbsp;&nbsp;up</button>
+                            <button type='button' className={styles.cta}>Sign&nbsp;&nbsp;up</button>
                         </div>
                         <span className={styles.sidehead}>Shipping information</span>
                         <div className={styles.shippingform}>
@@ -91,9 +102,8 @@ const Login = () => {
                             <label htmlFor="inpost" className={styles.label}>
                                 <input type="radio" name="pay" id="inpost" value='inpost' />
                                 <div className={styles.radiobtn}>
-                                    <div style={{}}>
-
-                                    </div>
+                                    <div style={{ backgroundImage: `url(${inpost})` }}
+                                        className={styles.deliverypic}></div>
                                     <div style={styles.price}>
                                         $20.00
                                     </div>
@@ -101,10 +111,9 @@ const Login = () => {
                             </label>
                             <label htmlFor="dpd" className={styles.label}>
                                 <input type="radio" name="pay" id="dpd" value='dpd' />
-                                <div
-                                    className={styles.radiobtn}>
-                                    <div style={{}}>
-                                    </div>
+                                <div className={styles.radiobtn}>
+                                    <div style={{ backgroundImage: `url(${dpd})` }}
+                                        className={styles.deliverypic}></div>
                                     <div style={styles.price}>
                                         $12.00
                                     </div>
@@ -113,7 +122,8 @@ const Login = () => {
                             <label htmlFor="dhl" className={styles.label}>
                                 <input type="radio" name="pay" id="dhl" value='dhl' />
                                 <div className={styles.radiobtn}>
-                                    <div style={{}}></div>
+                                    <div style={{ backgroundImage: `url(${dhl})` }}
+                                        className={styles.deliverypic}></div>
                                     <div className={styles.price}>
                                         $15.00
                                     </div>
@@ -122,7 +132,8 @@ const Login = () => {
                             <label htmlFor="fedex" className={styles.label}>
                                 <input type="radio" name="pay" id="fedex" value='fedex' />
                                 <div className={styles.radiobtn}>
-                                    <div style={{}}></div>
+                                    <div style={{ backgroundImage: `url(${fedex})` }}
+                                        className={styles.deliverypic}></div>
                                     <div className={styles.price}>
                                         $10.00
                                     </div>
@@ -130,23 +141,50 @@ const Login = () => {
                             </label>
                         </div>
                     </div>
-
-                    <div className={styles.cart}>
-                        <div className={styles.items}>
-                            <div className={styles.item}>
-
+                    <div>
+                        <h3 className={styles.sidehead}>Your Cart</h3>
+                        <div className={styles.cart}>
+                            <div className={styles.items}>
+                                <div className={styles.item}>
+                                    <div className={styles.profile}>
+                                        <img src={model1} alt="" />
+                                    </div>
+                                    <div className={styles.name}>
+                                        <p> T - Shirt Summer Vibes</p>
+                                        <p className={styles.id}>#261311</p>
+                                    </div>
+                                    <div className={styles.cost}>
+                                        <p>$89.99</p>
+                                    </div>
+                                </div>
+                                <div className={styles.item}>
+                                    <div className={styles.profile}>
+                                        <img src={model2} alt="" />
+                                    </div>
+                                    <div className={styles.name}>
+                                        <p>Basic Slim Fit T - Shirt</p>
+                                        <p className={styles.id}>#212315</p>
+                                    </div>
+                                    <div className={styles.cost}>
+                                        <p>$69.99</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={styles.item}>
-
+                            <div className={styles.total}>
+                                Total cost &nbsp;&nbsp;&nbsp;&nbsp; <span>$159.98</span>
                             </div>
-                        </div>
-                        <div className={styles.total}>
-
-                        </div>
-                        <div className={styles.message}>
-
+                            <div className={styles.message}>
+                                <FontAwesomeIcon icon={faTruck} />
+                                <span>
+                                    You are $30.03 away from free Shipping
+                                </span>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className={styles.submitbuttons}>
+                    <button type='button' className={styles.cta}>Continue Shopping</button>
+                    <button type='submit' className={styles.cta + " " + styles.selected}>Proceed to Payment</button>
                 </div>
             </form>
         </div>
